91abe107ca6332cb7299c206019479ad746a7e4c
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-11-16 11:18:57 -0300

    style: create app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { ChangeEvent, useState } from 'react';
 import './styles/app.css';
 
 function App() {
+    const [link, setLink] = useState<string>('');
+
+    const handleChangeLink = (e: ChangeEvent<HTMLInputElement>) => {
+        setLink(e.target.value);
+    };
+
     return (
         <div className="center">
             <main>
                 <h1>Gerador de links curtos</h1>
                 <p>Insira a url que você deseja encurtar no campo abaixo</p>
                 <form>
-                    <input type="text" placeholder="coloque sua url aqui!" />
-                    <button type="submit">Gerar Link</button>
+                    <input
+                        onChange={handleChangeLink}
+                        value={link}
+                        type="text"
+                        placeholder="coloque sua url aqui!"
+                    />
+                    <button className={link ? 'active' : ''} type="submit">
+                        Gerar Link
+                    </button>
                 </form>
             </main>
         </div>
